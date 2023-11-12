ca0ba2e3dccd602a1af621bad08f8c48e29280d8
Muslimbek <muslimbeksalimov0102@gmail.com>
2023-11-12 21:04:07 +0500

    JS Masala

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var elHeading = document.querySelector(".heading")
+
+var Username = Number(prompt("Qayerga ketyapsiz  To'lov pulini kiriting😎😎"))
+console.log(Username)
+  
+var pulyoq = 8317480
+
+if (Username > 8317480 | pulyoq < 8317480 ) {
+    elHeading.textContent = ("Tabriklaymiz oq yo'l, dengiz uzra parvoz qiling 👏👏 Alisher");
+} else if (pulyoq) {
+    elHeading.textContent = ("Tugadi Damingni ol Alish😁😁😁");
+}
